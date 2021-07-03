e30f104fd418ff87ead2b07505014601d59abacd
Kazuya-Pum <kazuya.pum@gmail.com>
2021-07-03 14:39:02 +0900

    POSTAPI作成

--- app/app.py
+++ app/app.py
@@ -1,4 +1,12 @@
 from system import system
+from bottle import route, run, template, request
 
-if __name__ == '__main__':
-    print(system.execute('Kazuya-Pum', 'LineLimiter', '.js'))
+
+@route('/divination', method='POST')
+def divination():
+	owner = request.forms.owner
+	repo = request.forms.repo
+	extension = request.forms.extension
+	return system.execute(owner, repo, extension)
+
+run(host='localhost', port='8080', debug=True)
