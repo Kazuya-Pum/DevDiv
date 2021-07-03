4e18b80ef38799438ae5ca74dee6bbacb7613923
Kazuya-Pum <kazuya.pum@gmail.com>
2021-07-03 18:35:30 +0900

    python 対応

--- system/astword/py/processor.py
+++ system/astword/py/processor.py
@@ -1,26 +1,21 @@
-from antlr4 import *
-if __name__ is not None and "." in __name__:
-    from .Python3Parser import Python3Parser
-    from .Python3Lexer import Python3Lexer
-    from .Python3Listener import Python3Listener
-else:
-    from Python3Parser import Python3Parser
-    from Python3Lexer import Python3Lexer
-    from Python3Listener import Python3Listener
+import ast
+import inspect
 
-
-class WordListener(Python3Listener):
+class CollectVarname(ast.NodeTransformer):
     def __init__(self):
         self.words = []
 
-    def enterArglist(self, ctx:Python3Parser.ArglistContext):
-        self.words.extend([child.getText() for child in ctx.getChildren()])
+    def visit_Name(self, node):
+        self.words.append(node.id)
+
+
+def collect_varname(node):
+    visitor = CollectVarname()
+    visitor.visit(node)
+    return visitor.words
 
 
 def get_words(filepath):
-    parser = Python3Parser(CommonTokenStream(Python3Lexer(FileStream(filepath, encoding="utf-8"))))
-    walker = ParseTreeWalker()
-    listener = WordListener()
-    walker.walk(listener, parser.file_input())
-
-    return listener.words
+    with open(filepath, 'r') as rf:
+        program = ast.parse(rf.read())
+        return collect_varname(program)
