fe6e0e3d167825302fbd7fef7afda9105430eec0
Kazuya-Pum <kazuya.pum@gmail.com>
2021-07-03 16:04:42 +0900

    setup gae

--- app/app.py
+++ app/app.py
@@ -1,12 +1,13 @@
 from system import system
-from bottle import route, run, template, request
+from bottle import Bottle
 
+app = Bottle()
 
 @route('/divination', method='POST')
 def divination():
-	owner = request.forms.owner
-	repo = request.forms.repo
-	extension = request.forms.extension
+	owner = app.request.forms.owner
+	repo = app.request.forms.repo
+	extension = app.request.forms.extension
 	return system.execute(owner, repo, extension)
 
-run(host='localhost', port='8080', debug=True)
+app.run(host='localhost', port='8080', debug=True)
